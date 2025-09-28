ec5701ac5ccfd8f0a425780f8d0b05229506ff50
Harshal Pawshekar <harshal.pawshekar@greytip.com>
2025-09-28 21:16:44 +0530

    Enhance `columnsTable` schema with constraints and timestamps, add Drizzle push script, and update Prettier config.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,14 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
 const db = drizzle(process.env.DATABASE_URL ?? "");
 
 export const columnsTable = pgTable("columns", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar(),
+  name: varchar().notNull().unique(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp()
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
 });
