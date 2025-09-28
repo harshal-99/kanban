c0e9757e1a39d0861f2ed7126f41b4ff5c4103b1
Harshal Pawshekar <harshal.pawshekar@greytip.com>
2025-09-28 17:16:57 +0530

    Set up Tailwind CSS, updated Drizzle ORM configuration, and added utility functions and dependencies

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
-import {drizzle} from 'drizzle-orm/vercel-postgres';
+import {drizzle} from 'drizzle-orm/neon-http';
 import {integer, pgTable, varchar} from "drizzle-orm/pg-core";
 
-const db = drizzle();
+const db = drizzle(process.env.DATABASE_URL ?? '');
 
 export const columnsTable = pgTable('columns', {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
