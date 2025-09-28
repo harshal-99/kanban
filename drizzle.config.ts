e928be3ccde6750e725a3c16e8de4c26de90325f
Harshal Pawshekar <harshal.pawshekar@greytip.com>
2025-09-28 22:14:20 +0530

    Add environment validation for `DATABASE_URL` and remove unused `@vercel/postgres` dependency

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
+if (!process.env.DATABASE_URL)
+  throw new Error(
+    "DATABASE_URL is not defined. Please define it in your .env file.",
+  );
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? "",
+    url: process.env.DATABASE_URL,
   },
   verbose: true,
 });
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,12 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
-const db = drizzle(process.env.DATABASE_URL ?? "");
+if (!process.env.DATABASE_URL)
+  throw new Error(
+    "DATABASE_URL is not defined. Please define it in your .env file.",
+  );
+
+const db = drizzle(process.env.DATABASE_URL);
 
 export const columnsTable = pgTable("columns", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
